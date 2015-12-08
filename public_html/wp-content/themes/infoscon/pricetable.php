947b90870799f40a4b7fc3dcacc57ea19ba32b6e
unknown <scoutrul@mail.ru>
2015-12-08 04:37:52 +0300

    price table

--- public_html/wp-content/themes/infoscon/pricetable.php
+++ public_html/wp-content/themes/infoscon/pricetable.php
@@ -1,6 +1,6 @@
 
 	<h2>Таблица услуг</h2>
-	<table class="price">
+	<table class="price" cellpadding="0" cellspacing="0" border="0">
 	<?php
 	wp_reset_query(); ?>
 
@@ -17,46 +17,48 @@
 	foreach($categories_in_table as $categoryTab) {
 	?>
 
-	<? echo '
-			<tr>
-				<th colspan="4">';
-		echo  '<h3>'.$categoryTab->name.'</h3>';
-		echo '
-				</th>
-			</tr>
-		</thead>';
-	?>
 
+		
 
+		<? echo '
+				<tr>
+					<th colspan="4" class="price-header">';
+			echo  '<h2 class="h" >'.$categoryTab->name.'</h3>';
+			echo '
+					</th>
+				</tr>
+			</thead>';
+		?>
 
 			<?	$IDcatTab = 'cat='.$categoryTab->term_id;
 				query_posts($IDcatTab); 
 				while (have_posts()) : the_post(); ?>
 
 
-		<tbody>
-
-			<tr>
-				<td><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-				</td>
-				<td>
-				
-
-				</td>
-				<td><?echo get_post_meta($post->ID, 'days', true);?></td>
-				<td><?echo get_post_meta($post->ID, 'price', true);?></td>
-			</tr>
-		</tbody>
 
 
+			<tbody class="price-section-in">
 
-		<?php endwhile; ?>
+				<tr>
+					<td><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+					</td>
+					<td><?php the_excerpt(); ?>
+					</td>
+					<td><?echo get_post_meta($post->ID, 'days', true);?></td>
+					<td><?echo get_post_meta($post->ID, 'price', true);?></td>
+				</tr>
+			</tbody>
 
-		<? echo '
-		<tfoot>
-			<tr>
-				<th colspan="4">---</th>
-			</tr>
-		</tfoot>';?>
-<? }?>
+
+
+			<?php endwhile; ?>
+
+			<? echo '
+			<tfoot>
+				<tr>
+					<th colspan="4" class="price-section-end"></th>
+				</tr>
+			</tfoot>';?>
+	<? }?>
+
 	</table>
